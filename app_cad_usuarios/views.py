b2a961d92a00661107b8a58f7251448b499382c2
CaioSimioni <83130766+CaioSimioni@users.noreply.github.com>
2024-03-31 16:51:17 -0300

    Primeira vez funcionando.

--- app_cad_usuarios/views.py
+++ app_cad_usuarios/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .models import Usuario
 
 # Create your views here.
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def listagem_usuarios(request):
-    pass
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    return render(request, 'usuarios/usuarios.html', usuarios)
+
