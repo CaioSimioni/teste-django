a7a12cd47b6e4e774f7223afbcafd48c63acd2d1
CaioSimioni <83130766+CaioSimioni@users.noreply.github.com>
2024-03-31 16:24:32 -0300

    update home

--- projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/urls.py
@@ -5,5 +5,6 @@ from app_cad_usuarios import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('usuarios/', views.listagem_usuarios, name='listagem_usuarios'),
     path('admin/', admin.site.urls, name='admin'),
 ]
